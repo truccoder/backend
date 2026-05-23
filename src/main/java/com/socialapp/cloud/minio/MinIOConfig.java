8aa3ff32a906c69d488336276fe886da9211d2a0
Trần Phú Thịnh <137867288+thinhtpts00865@users.noreply.github.com>
2026-05-23 20:36:58 +0700

    [lyvqJeN6]: [BE] Create MinIO service

--- src/main/java/com/socialapp/cloud/minio/MinIOConfig.java
+++ src/main/java/com/socialapp/cloud/minio/MinIOConfig.java
@@ -1,1 +1,22 @@
+package com.socialapp.cloud.minio;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.minio.MinioClient;
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+@Data
+public class MinIOConfig {
+  private String url;
+  private String accessKey;
+  private String secretKey;
+
+  @Bean
+  public MinioClient minioClient() {
+    return MinioClient.builder().endpoint(url).credentials(accessKey, secretKey).build();
+  }
+}
