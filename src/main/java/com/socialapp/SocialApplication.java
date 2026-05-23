7672abcf8ac471a1ab2804c2007a6bf18d6dc7f3
truc2001 <thn2858@yum.com>
2026-05-23 17:01:54 +0700

    [Zat818UJ]: [Infra] Integrate Github and Zalo with Trello

--- src/main/java/com/socialapp/SocialApplication.java
+++ src/main/java/com/socialapp/SocialApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocialApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SocialApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SocialApplication.class, args);
+  }
 }
